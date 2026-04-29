df71bdff74b28c23cc61f12d4964b12e455d50fa
费文轩 <feiwenxuan@ultrapower.com.cn>
2026-04-29 15:26:46 +0800

    进行项目的规范重构

--- src/main/java/com/huashuo/task/entity/TaskEntity.java
+++ src/main/java/com/huashuo/task/entity/TaskEntity.java
@@ -15,8 +15,6 @@ public class TaskEntity {
     @TableId(value = "task_id", type = IdType.AUTO)
     private Long taskId;
 
-    private Long userId;
-
     private Long projectId;
 
     private String taskType;
