eed714800ce170e979b9b5ae171958ea46e225db
hbaoquan <hbaoquan@gmail.com>
2026-05-04 16:42:41 +0800

    修改了模型的调用

---
+++ src/main/java/com/huashuo/common/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.huashuo.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean("writerTaskExecutor")
+    public Executor writerTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(20);
+        executor.setThreadNamePrefix("writer-task-");
+        executor.initialize();
+        return executor;
+    }
+}
