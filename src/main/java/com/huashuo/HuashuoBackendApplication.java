1ed09fd2425d6bd0c680179c5f287136243af6fd
费文轩 <feiwenxuan@ultrapower.com.cn>
2026-05-15 10:36:41 +0800

    Enable RabbitMQ task consumers

--- src/main/java/com/huashuo/HuashuoBackendApplication.java
+++ src/main/java/com/huashuo/HuashuoBackendApplication.java
@@ -6,11 +6,13 @@ import com.huashuo.upload.tos.VolcengineTosProperties;
 import com.huashuo.video.config.ViduDigitalHumanProperties;
 import com.huashuo.voice.config.VolcengineTtsProperties;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableRabbit
 @MapperScan({
         "com.huashuo.upload.mapper",
         "com.huashuo.asset.mapper",
